9ea873d1219279adcdaec9c7ad4fcb25f85ca3c9
vnprc <vnprc@protonmail.com>
2025-05-17 17:32:46 -0400

    try electrs-rest fork with a proxy
    still broken though...why??

--- devenv.nix
+++ devenv.nix
@@ -43,8 +43,21 @@ in
 
   processes.esplora-ui.exec = ''
     ${pkgs.nodePackages.serve}/bin/serve \
-      -l 5000 \
+      -l 5001 \
       -s esplora-frontend
+  '';
+
+  processes.proxy.exec = ''
+    cat > /tmp/Caddyfile <<'CFG'
+    {
+      auto_https off
+    }
+    :5000 {
+      reverse_proxy /api/* 127.0.0.1:3000
+      reverse_proxy        127.0.0.1:5001
+    }
+    CFG
+    ${pkgs.caddy}/bin/caddy run --config /tmp/Caddyfile --adapter caddyfile
   '';
 
   enterShell = ''
--- flake.nix
+++ flake.nix
@@ -6,12 +6,19 @@
     flake-utils.url = "github:numtide/flake-utils";
     devenv.url = "github:cachix/devenv";
     garrys-mod.url = "github:vnprc/bitcoin-garrys-mod";
+    electrs-rest.url = "github:mempool/electrs?ref=mempool";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, devenv, garrys-mod, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, electrs-rest, devenv, garrys-mod, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlay = (final: prev: {
+          electrs = electrs-rest.packages.${system}.default.overrideAttrs (_: {
+            doCheck   = false;   # nuke every test
+            dontCheck = true;
+          });
+        });
+        pkgs = import nixpkgs { inherit system overlays = [ overlay ]; };
         bitcoind = garrys-mod.packages.${system}.default;
       in {
         packages.default = bitcoind;
