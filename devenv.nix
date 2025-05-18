d46ed91f6de3e3c915824658067b59b98ffae397
vnprc <vnprc@protonmail.com>
2025-05-18 11:52:28 -0400

    get electrs indexing blocks
    switch to blockstream/electrs fork
    recompile electrs-esplora-server target
    turn on info logging
    reverse proxy still broken though

--- devenv.nix
+++ devenv.nix
@@ -3,14 +3,26 @@
 let
   garrys-mod = builtins.getFlake "github:vnprc/bitcoin-garrys-mod";
   bitcoind = garrys-mod.packages.${pkgs.system}.gmodBitcoind;
+
+  electrs-flake = builtins.getFlake "github:blockstream/electrs?ref=new-index";
+  electrs-bin = electrs-flake.packages.${pkgs.system}.bin;
+
+  # Re-compile electrs-esplora-server target
+  electrs-esplora =
+    electrs-bin.overrideAttrs (old: {
+      pname           = "electrs-esplora-server";
+      # crane respects cargoExtraArgs
+      cargoExtraArgs  = "--bin electrs-esplora-server";
+      doCheck         = false;      # skip the flaky test_rest integration test
+    });
+
   datadir = "./data";
   bitcoinConf = ./config/bitcoin.conf;
-  electrsConf = ./electrs.toml;
 in
 {
   packages = [
     bitcoind
-    pkgs.electrs
+    electrs-esplora
     pkgs.nodejs
     pkgs.nodePackages.npm
     pkgs.nodePackages.serve
@@ -34,11 +46,22 @@ in
       sleep 1
     done
 
-    ${pkgs.electrs}/bin/electrs \
+    echo "Waiting for bitcoind RPC to become ready..."
+    until ${bitcoind}/bin/bitcoin-cli \
+            -datadir=${datadir} -conf=${bitcoinConf} -regtest \
+            getblockchaininfo >/dev/null 2>&1; do
+        sleep 1
+    done
+
+    COOKIE_PATH=${datadir}/regtest/.cookie
+
+    ${electrs-esplora}/bin/electrs \
       --network regtest \
       --daemon-dir ${datadir} \
+      --daemon-rpc-addr 127.0.0.1:18443 \
+      --cookie $(cat $COOKIE_PATH) \
       --db-dir ./electrs-db \
-      --conf ${electrsConf}
+      --http-addr 0.0.0.0:3000 -vvv
   '';
 
   processes.esplora-ui.exec = ''
@@ -53,7 +76,8 @@ in
       auto_https off
     }
     :5000 {
-      reverse_proxy /api/* 127.0.0.1:3000
+      reverse_proxy /blocks/* 127.0.0.1:3000
+      reverse_proxy /mempool/* 127.0.0.1:3000
       reverse_proxy        127.0.0.1:5001
     }
     CFG
