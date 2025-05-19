c3b3f6ef973c1b50be1e0aa167031e051d579e95
vnprc <vnprc@protonmail.com>
2025-05-19 14:39:41 -0400

    mine and spend a CTV coinbase transaction
    TODO: read the code

--- scripts/src/mine_ctv_coinbase.rs
+++ scripts/src/mine_ctv_coinbase.rs
@@ -3,13 +3,13 @@ use std::str::FromStr;
 
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use bitcoin::{
-    Address, Amount, Network, Transaction, TxOut,
-    consensus::Encodable,
+    Address, Amount, Network, Transaction, TxOut, TxIn, OutPoint,
+    consensus::{Encodable, encode::serialize_hex},
     hashes::{sha256, Hash},
     key::{Keypair, Secp256k1},
     opcodes::all::{OP_RETURN, OP_NOP4},
     script::Builder,
-    taproot::{TaprootBuilder, TaprootSpendInfo},
+    taproot::{TaprootBuilder, TaprootSpendInfo, LeafVersion},
     Opcode, XOnlyPublicKey, Sequence,
 };
 
@@ -21,84 +21,165 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Auth::CookieFile(Path::new("./data/regtest/.cookie").to_path_buf()),
     )?;
 
-    // 👇 ensure wallet is created and loaded
-    let wallet_name = "devwallet"; // 👈 changed
-    let _ = rpc.create_wallet(wallet_name, None, None, None, None); // 👈 changed
+    ensure_wallet(&rpc, "devwallet")?;
 
-    let loaded_wallets = rpc.list_wallets()?; // 👈 changed
-    if !loaded_wallets.iter().any(|w| w == wallet_name) { // 👈 changed
-        rpc.load_wallet(wallet_name)?; // 👈 changed
-    }
-
-    // 👇 reconnect to wallet RPC endpoint
-    let rpc = Client::new( // 👈 changed
-        &format!("http://127.0.0.1:18443/wallet/{}", wallet_name), // 👈 changed
-        Auth::CookieFile(Path::new("./data/regtest/.cookie").to_path_buf()), // 👈 changed
+    let rpc = Client::new(
+        "http://127.0.0.1:18443/wallet/devwallet",
+        Auth::CookieFile(Path::new("./data/regtest/.cookie").to_path_buf()),
     )?;
 
-    let ctv_spend_address = rpc.get_new_address(None, None)?
-        .require_network(Network::Regtest)?;
+    let secp = Secp256k1::new();
+    let keypair = Keypair::new(&secp, &mut rand::thread_rng());
+    let (xonly_pubkey, _) = XOnlyPublicKey::from_keypair(&keypair);
 
-    let anchor_address = Address::from_str("bcrt1pfeesnyr2tx")?
-        .require_network(Network::Regtest)?;
+    // hardcoded config
+    let spend_value = 1577;
+    let anchor_value = 240;
+    let fee = 500;
 
-    let outputs = [
-        TxOut {
-            value: Amount::from_sat(1577 - 240),
-            script_pubkey: ctv_spend_address.script_pubkey(),
-        },
-        TxOut {
-            value: Amount::from_sat(240),
-            script_pubkey: anchor_address.script_pubkey(),
-        },
-        TxOut {
-            value: Amount::from_sat(0),
-            script_pubkey: Builder::new()
-                .push_opcode(OP_RETURN)
-                .push_slice(b"\xf0\x9f\xa5\xaa \xe2\x9a\x93 \xf0\x9f\xa5\xaa")
-                .into_script(),
-        },
+    let ctv_spend_address = rpc.get_new_address(None, None)?.require_network(Network::Regtest)?;
+    let anchor_address = Address::from_str("bcrt1pfeesnyr2tx")?.require_network(Network::Regtest)?;
+    let change_address = rpc.get_new_address(None, None)?.require_network(Network::Regtest)?;
+
+    // ⚠️ Mine a dummy block to get the actual coinbase value
+    let dummy_address = rpc.get_new_address(None, None)?.require_network(Network::Regtest)?;
+    let dummy_block = rpc.generate_to_address(1, &dummy_address)?[0];
+    let dummy_txid = rpc.get_block(&dummy_block)?.txdata[0].txid();
+    let dummy_coinbase_tx: Transaction = rpc.get_raw_transaction(&dummy_txid, None)?;
+    let actual_coinbase_value = dummy_coinbase_tx.output[0].value.to_sat();
+
+    // Now construct spend tx and CTV tree with real input amount
+    let (taproot_info, ctv_address, mut spend_tx, ctv_script) = build_ctv_contract(
+        &secp,
+        xonly_pubkey,
+        actual_coinbase_value,
+        spend_value,
+        anchor_value,
+        fee,
+        &ctv_spend_address,
+        &anchor_address,
+        &change_address,
+    )?;
+
+    // Mine coinbase to actual CTV address
+    println!("Mining to CTV contract address: {}", ctv_address);
+    let coinbase_block = rpc.generate_to_address(1, &ctv_address)?[0];
+    let coinbase_txid = rpc.get_block(&coinbase_block)?.txdata[0].txid();
+
+    // Mature the coinbase
+    rpc.generate_to_address(100, &ctv_spend_address)?;
+
+    // Fill in prevout
+    spend_tx.input[0].previous_output = OutPoint {
+        txid: coinbase_txid,
+        vout: 0,
+    };
+
+    // Finalize witness
+    let ctrl_block = taproot_info
+        .control_block(&(ctv_script.clone(), LeafVersion::TapScript))
+        .unwrap();
+
+    spend_tx.input[0].witness.push(ctv_script.into_bytes());
+    spend_tx.input[0].witness.push(ctrl_block.serialize());
+
+    // Broadcast
+    let tx_hex = serialize_hex(&spend_tx);
+    println!("Spending tx: {tx_hex}");
+    let txid = rpc.send_raw_transaction(tx_hex)?;
+    println!("Broadcasted txid: {txid}");
+
+    Ok(())
+}
+
+fn ensure_wallet(rpc: &Client, wallet_name: &str) -> Result<(), bitcoincore_rpc::Error> {
+    let _ = rpc.create_wallet(wallet_name, None, None, None, None);
+    if !rpc.list_wallets()?.contains(&wallet_name.to_string()) {
+        rpc.load_wallet(wallet_name)?;
+    }
+    Ok(())
+}
+
+fn build_ctv_contract(
+    secp: &Secp256k1<bitcoin::secp256k1::All>,
+    xonly: XOnlyPublicKey,
+    input_value_sat: u64,
+    spend_value: u64,
+    anchor_value: u64,
+    fee: u64,
+    ctv_spend_address: &Address,
+    anchor_address: &Address,
+    change_address: &Address,
+) -> Result<(TaprootSpendInfo, Address, Transaction, bitcoin::ScriptBuf), Box<dyn std::error::Error>> {
+    let ctv_output = TxOut {
+        value: Amount::from_sat(spend_value - anchor_value),
+        script_pubkey: ctv_spend_address.script_pubkey(),
+    };
+    let anchor_output = TxOut {
+        value: Amount::from_sat(anchor_value),
+        script_pubkey: anchor_address.script_pubkey(),
+    };
+    let op_return_output = TxOut {
+        value: Amount::from_sat(0),
+        script_pubkey: Builder::new()
+            .push_opcode(OP_RETURN)
+            .push_slice(b"\xf0\x9f\xa5\xaa \xe2\x9a\x93 \xf0\x9f\xa5\xaa")
+            .into_script(),
+    };
+    let change_value_sat = input_value_sat - spend_value - fee;
+    let change_output = TxOut {
+        value: Amount::from_sat(change_value_sat),
+        script_pubkey: change_address.script_pubkey(),
+    };
+
+    let outputs = vec![
+        ctv_output.clone(),
+        anchor_output.clone(),
+        op_return_output.clone(),
+        change_output.clone(),
     ];
 
     let ctv_hash = calc_ctv_hash(&outputs, None);
-    let taproot_info = create_ctv_address(ctv_hash);
+    let ctv_script = Builder::new()
+        .push_slice(&ctv_hash)
+        .push_opcode(OP_CTV)
+        .into_script();
+
+    let taproot_info = TaprootBuilder::new()
+        .add_leaf(0, ctv_script.clone())
+        .unwrap()
+        .finalize(secp, xonly)
+        .unwrap();
+
     let ctv_address = Address::p2tr_tweaked(taproot_info.output_key(), Network::Regtest);
 
-    println!("Mining to CTV contract address: {}", ctv_address);
+    let spend_tx = Transaction {
+        version: bitcoin::transaction::Version(3),
+        lock_time: bitcoin::absolute::LockTime::ZERO,
+        input: vec![TxIn {
+            previous_output: OutPoint::null(),
+            sequence: Sequence::ENABLE_RBF_NO_LOCKTIME,
+            witness: bitcoin::Witness::default(),
+            script_sig: bitcoin::ScriptBuf::new(),
+        }],
+        output: outputs,
+    };
 
-    let block_hashes = rpc.generate_to_address(1, &ctv_address)?;
-    let block_hash = block_hashes[0];
-
-    let block = rpc.get_block(&block_hash)?;
-    let coinbase_txid = block.txdata[0].txid();
-
-    println!("Coinbase txid: {}", coinbase_txid);
-
-    let coinbase_tx: Transaction = rpc.get_raw_transaction(&coinbase_txid, None)?;
-    let ctv_output_index = coinbase_tx
-        .output
-        .iter()
-        .position(|o| o.script_pubkey == ctv_address.script_pubkey())
-        .expect("CTV output not found in coinbase");
-
-    println!("CTV output index: {}", ctv_output_index);
-    println!("Output value: {} sats", coinbase_tx.output[ctv_output_index].value);
-
-    Ok(())
+    Ok((taproot_info, ctv_address, spend_tx, ctv_script))
 }
 
 fn calc_ctv_hash(outputs: &[TxOut], timeout: Option<u32>) -> [u8; 32] {
     let mut buffer = Vec::new();
     buffer.extend(3_i32.to_le_bytes()); // version
     buffer.extend(0_i32.to_le_bytes()); // locktime
-    buffer.extend(1_u32.to_le_bytes()); // inputs len
+    buffer.extend(1_u32.to_le_bytes()); // input count
 
     let seq = if let Some(timeout_value) = timeout {
         sha256::Hash::hash(&Sequence(timeout_value).0.to_le_bytes())
     } else {
         sha256::Hash::hash(&Sequence::ENABLE_RBF_NO_LOCKTIME.0.to_le_bytes())
     };
-    buffer.extend(seq.to_byte_array()); // sequences
+    buffer.extend(seq.to_byte_array());
 
     buffer.extend((outputs.len() as u32).to_le_bytes());
 
@@ -106,23 +187,9 @@ fn calc_ctv_hash(outputs: &[TxOut], timeout: Option<u32>) -> [u8; 32] {
     for o in outputs {
         o.consensus_encode(&mut output_bytes).unwrap();
     }
-    buffer.extend(sha256::Hash::hash(&output_bytes).to_byte_array()); // outputs hash
+    buffer.extend(sha256::Hash::hash(&output_bytes).to_byte_array());
 
-    buffer.extend(0_u32.to_le_bytes()); // inputs index
+    buffer.extend(0_u32.to_le_bytes()); // input index
 
     sha256::Hash::hash(&buffer).to_byte_array()
-}
-
-fn create_ctv_address(ctv_hash: [u8; 32]) -> TaprootSpendInfo {
-    let secp = Secp256k1::new();
-    let keypair = Keypair::new(&secp, &mut rand::thread_rng());
-    let (xonly_pubkey, _parity) = XOnlyPublicKey::from_keypair(&keypair);
-
-    let script = Builder::new()
-        .push_slice(&ctv_hash)
-        .push_opcode(OP_CTV)
-        .into_script();
-
-    let builder = TaprootBuilder::new().add_leaf(0, script).unwrap();
-    builder.finalize(&secp, xonly_pubkey).unwrap()
 }
